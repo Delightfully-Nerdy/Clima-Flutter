ab433f4c391f2576a23ff6a7c3f26d37b265d3de
DalitsoKasonde <dkasonde11@gmail.com>
2021-03-03 11:25:48 +0200

    :

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -5,6 +5,14 @@ const apiKey = '5f3b441e8ee65a50bb8d72b6dde3142e';
 const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric');
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
